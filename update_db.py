59634eda81d8f24a1fd80eb771c02096ce022198
Alyssa Norrenberns <alyssa.norrenberns@gmail.com>
2025-02-22 17:38:57 -0600

    created update_db.py
    created the following tables:
    - programs
    - required_class
    - elective_class
    - gen_eds

---
+++ update_db.py
@@ -0,0 +1,69 @@
+
+import sqlite3
+
+conn = sqlite3.connect("college.db")
+cursor = conn.cursor()
+
+# create programs table
+cursor.execute(''' 
+        CREATE TABLE IF NOT EXISTS programs (
+            program_type TEXT NOT NULL,
+            name TEXT NOT NULL,
+            PRIMARY KEY (program_type)
+        )
+    ''')
+
+#  create required_class table
+cursor.execute(''' 
+    CREATE TABLE IF NOT EXISTS required_class (
+        class_id TEXT NOT NULL,
+        program_type TEXT NOT NULL,
+        rec_semester INTEGER,
+        PRIMARY KEY (class_id),
+        FOREIGN KEY (class_id) REFERENCES class(id) ON DELETE CASCADE,
+        FOREIGN KEY (program_type) REFERENCES programs(program_type) ON DELETE CASCADE
+    )
+''')
+
+#  create elective_class table
+cursor.execute(''' 
+    CREATE TABLE IF NOT EXISTS elective_class (
+        class_id TEXT NOT NULL,
+        program_type TEXT NOT NULL,
+        rec_semester INTEGER,
+        PRIMARY KEY (class_id),
+        FOREIGN KEY (class_id) REFERENCES class(id) ON DELETE CASCADE,
+        FOREIGN KEY (program_type) REFERENCES programs(program_type) ON DELETE CASCADE
+    )
+''')
+
+# create gen_eds table
+cursor.execute(''' 
+    CREATE TABLE IF NOT EXISTS gen_eds (
+        class_id TEXT NOT NULL,
+        math_basic INTEGER,
+        english_basic INTEGER,
+                
+        gov INTEGER,
+        american_history INTEGER,
+                
+        writing_intensive INTEGER,
+                
+        humanities INTEGER,
+        fine_arts INTEGER,
+                
+        bio_sci INTEGER,
+        physical_sci INTEGER,
+        
+        behavioral_sci INTEGER,
+        social_sci INTEGER,
+        
+        PRIMARY KEY (class_id)
+        FOREIGN KEY (class_id) REFERENCES class(id) ON DELETE CASCADE
+    )
+''')
+
+conn.commit()
+conn.close()   
+print("Database updated successfully!")
+
